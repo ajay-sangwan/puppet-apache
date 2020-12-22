0fe279805d58ec3b8a2682104a47505b1f3b66d0
root <root@3386775c2a1c.mylabserver.com>
2020-12-22 20:25:39 +0000

    adding config files

---
+++ manifests/config.pp
@@ -0,0 +1,16 @@
+# @summary - Manages configuration files for Apache 
+#
+# A description of what this class does
+#
+# @example
+#   include apache::config
+class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+}
